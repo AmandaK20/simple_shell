14acae2411817365a4b86fedf36299659fd91a39
Amanda <Amanda@gmail.com>
2023-07-19 14:33:48 -0700

    Hello.c

---
+++ Hello.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - program that takes no argument
+ * Return: Always 0 (Success);
+ *
+ */
+
+int main(void)
+{
+	printf("My betty pass code");
+
+	return (0);
+
+}
