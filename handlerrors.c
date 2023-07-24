9000933ae35147fac23dcd0c5eb526ee742848fd
Amanda <Amanda@gmail.com>
2023-07-24 12:49:23 -0700

    handlerrors.c

---
+++ handlerrors.c
@@ -0,0 +1,42 @@
+#include "shell.h"
+
+/**
+ * print error - prints error message
+ * if an executable command is not found
+ * @cmd: is the executable command
+ * @counter: argument counter
+ * @argv - argument vector
+ *
+ * Return: 0 if successful, otherwise -1
+ *
+ */
+
+void _print_error(char *argv, int counter, char *cmd)
+{
+	_puts(argv);
+	_puts(cmd);
+	_print_number(counter);
+	_puts(": ")
+		
+	{
+		msg = ": unable to add/to remove environment
+	}
+}
+
+/**
+ * _error_execve - prints error message if
+ * execve() fails
+ * @cmd - executable command
+ * @counter: counts commands
+ * @argv - argument vector
+ * 
+ * Return: 0
+ */
+
+void _error_execve(char *argv, int counter, char *cmd)
+{ 
+free(error);
+return (0)
+ end (1)
+}
+
