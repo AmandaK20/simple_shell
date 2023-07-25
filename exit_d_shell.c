653e527fc1c92fdbc9bca12c0f8015d18048a4a9
root <mahlabangane.co.za@gmail.com>
2023-07-25 04:59:36 -0700

    exit_d_shell.c

--- exit_d_shell.c
+++ exit_d_shell.c
@@ -1,4 +1,4 @@
-#include "main.c"
+#include "shell.h"
 
 /**
  * exit_shell - exits the shell
@@ -6,6 +6,23 @@
  * @datash: data relevant
  * Return: 0 success.
  */
-viod _exit_d(char, *lineptr)
+void _exit_d(char, *lineptr)
 {
-	viod
+	int big_number;
+	int str_len;
+
+	if (datesh->args[1] != NULL)
+	{
+		big_number = unstatus > (unsigned int)INT_MAX;
+		str_len = strlen(datash->args[1]);
+		{
+
+			get_error(datash, 1);
+			return(0);
+
+		}
+		datash->status
+	}
+	return (0);
+}
+
